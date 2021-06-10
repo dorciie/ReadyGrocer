d5e308a0f8819d87e01ef7084173305d98b55e58
Hasya <61346089+Hasya00@users.noreply.github.com>
2021-06-11 01:57:14 +0800

    Update sidebar.blade.php

--- resources/views/shop/layouts/sidebar.blade.php
+++ resources/views/shop/layouts/sidebar.blade.php
@@ -5,7 +5,7 @@
         <nav class="sidebar-nav">
             <ul id="sidebarnav" class="pt-4">
                 <li class="sidebar-item"> <a class="sidebar-link waves-effect waves-dark sidebar-link"
-                        href="{{url('shop/shopdashboard')}}" aria-expanded="false"><i class="mdi mdi-view-dashboard"></i><span
+                        href="{{url('shop/dashboard')}}" aria-expanded="false"><i class="mdi mdi-view-dashboard"></i><span
                             class="hide-menu">Dashboard</span></a></li>
                 <li class="sidebar-item"> <a class="sidebar-link has-arrow waves-effect waves-dark"
                         href="javascript:void(0)" aria-expanded="false"><i class="mdi mdi-table-large"></i><span
